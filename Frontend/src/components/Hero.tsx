5f422cee7f1ff5b25a983ad836ebd3ed60e50afc
SakshamKaundal <137274099+SakshamKaundal@users.noreply.github.com>
2024-08-17 11:48:49 +0530

    new UI changes

--- Frontend/src/components/Hero.tsx
+++ Frontend/src/components/Hero.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 function Hero() {
+  
   return (
-    <div className='border-b-2 border-black pb-7'>
-        <div className='font-bold text-7xl p-5 ml-7 mt-3 '>Ace your</div>
-        <div className='font-bold text-7xl p-5 ml-7'>Online Assessment with</div>
-        <div className='font-bold text-7xl p-5 ml-7'>Confidence</div>
+    
+    <div className='border-b-2 border-black bg-zinc-900 pb-6 h-[64vh]'>
+        <div className='font-bold font-semibold text-white text-7xl p-5 ml-7 '>Ace your</div>
+        <div className='font-bold font-semibold  text-white text-7xl p-5 ml-7'>Online Assessment with</div>
+        <div className='font-bold font-extralight  text-yellow-300 text-7xl p-5 ml-7' style={{ fontFamily: '"Dancing Script", cursive' }}>Confidence</div>
     </div>
   )
 }
--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -4,20 +4,20 @@ function Navbar() {
 
   const navigate = useNavigate();
   return (
-    <div className='w-full h-16 border-black border-b-2 flex justify-between items-center'>
+    <div className='w-full h-16 border-black border-b-1 flex justify-between items-center bg-inherit'>
       <div >
         <div className="flex">
-          <img src="../../images/placement.png" alt="logo" width={70} />
-          <div className=" text-2xl font-bold flex justify-center items-center">
-            <Link to={"/"}>Placement Hub</Link>
+          
+        <div className=" text-2xl font-bold flex justify-center items-center">
+            <Link to={"/"}><img src="../../images/thelogo.png" alt="logo" width={220} style={{ paddingLeft: '20px' }} /></Link>
           </div>
         </div>
       </div>
-      <div className="flex gap-5 justify-content items-center pr-5">
+      <div className="flex gap-10 justify-content items-center pr-5">
         {['Home','Companies','Archives','About Us'].map((item)=>{
           return <div className="text-lg ">{item}</div>
         })}
-        <button onClick={()=>{navigate("/signup")}} className="bg-black text-white px-3 py-2 rounded-xl">Sign Up</button>
+        <button onClick={()=>{navigate("/signup")}} className="bg-black text-yellow-200 px-3 py-2 rounded-xl">Sign Up</button>
       </div>
     </div>
   )
